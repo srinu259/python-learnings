8d5acdde8ca173a069a12e21761a958e68a7872e
Madhav <Madhav.Srinivas@gmail.com>
2021-10-10 15:30:44 +0530

    dictionary updates

--- dictionary-and-sets/dict-type.py
+++ dictionary-and-sets/dict-type.py
@@ -16,18 +16,19 @@ print(e)
 f = dict({'one': 1, 'three': 3}, two=2)
 print(f)
 
-a == b == c == d == e == f
+print(a == b == c == d == e == f)
 
 g = {(1, "Nightflight"): 1}
 print(g)
 
+# a dictionary key has to be immutable, else it is considered invalid
 # h = {(1, ["a", "b", "c"]): 1}
 # print(h)
 
 
 # tuple is always immutable, but if the tuple contains mutable objects, then those objects can be changed
 tup1 = (1, ["a", "b"])
-print(hash(tup1))
+#print(hash(tup1))
 tup1[1].append("c")
 for i in tup1:
     print(i)
