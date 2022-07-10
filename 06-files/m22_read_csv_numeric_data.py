abbd2f4b7a753b371deeba394fb6513b84e725f5
Madhav <Madhav.Srinivas@gmail.com>
2022-07-10 14:54:42 +0530

    Learning again!

--- 06-files/m22_read_csv_numeric_data.py
+++ 06-files/m22_read_csv_numeric_data.py
@@ -1,2 +1,11 @@
+# quoting provides you the flexibility to set the numeric value in the data
+# by default csv.reader converts every field to string, this may not be idea situation if you need numbers
+# csv.QUOTE_NONNUMERIC means all non-numeric columns are converted to numbers (float)
+# So if you have a string column that does not have quotes, it will lead to error
 import csv
 
+file_name = "m21_cereal_grains.csv"
+with open(file_name, 'r', encoding='utf-8') as file:
+    reader = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
+    for row in reader:
+        print(row)
---
+++ 06-files/m23_csv_dialect.py
@@ -0,0 +1,39 @@
+import csv
+
+# csv can read with any delimiter, not with just comma
+# just use the delimiter option and provide the delimiter in your case
+
+file_name = "m7_country_info.txt"
+with open(file_name, 'r', encoding='utf-8') as file:
+    reader = csv.reader(file, delimiter='|')
+    for row in reader:
+        print(row)
+
+print("*"*80)
+
+# there can be cases where you have different delimiters in different files
+# in such cases, you can use dialect
+# csv has Sniffer class that helps you to identify the delimiter in your data
+# in the below example we are loading the full data and passing to the Sniffer
+# we can also pass a sample data instead of full data to identify the delimiter
+
+file_name = "m7_country_info.txt"
+with open(file_name, 'r', encoding='utf-8') as file:
+    country_data = file.read()
+    file.seek(0)
+    country_dialect = csv.Sniffer().sniff(country_data)
+    reader = csv.reader(file, dialect=country_dialect)
+    for row in reader:
+        print(row)
+
+print("*"*80)
+# Get all the attributes of the dialect
+# dialect is an object that encompasses all the csv properties
+attributes = ['delimiter', 'quoting', 'quotechar', 'escapechar',
+              'doublequote', 'lineterminator', 'skipinitialspace']
+for attribute in attributes:
+    print(f"{attribute}: {getattr(country_dialect, attribute)}")
+
+print("*"*80)
+for attribute in attributes:
+    print(f"{attribute}: {repr(getattr(country_dialect, attribute))}")
