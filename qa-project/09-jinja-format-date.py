4e614f742ab2b9acaf1dab957a55f19ebbf3894e
Madhav <Madhav.Srinivas@gmail.com>
2024-12-26 01:26:30 +0530

    Learning again!

--- qa-project/09-jinja-format-date.py
+++ qa-project/09-jinja-format-date.py
@@ -1,29 +1,44 @@
 from jinja2 import Template, Environment, Undefined
 from datetime import datetime
 
+
 # 01/12/2024 1733011200000
-def datetime_format(value, format):
+def format_date_time(value, format=""):
     if isinstance(value, str):
         value = datetime.strptime(value, "%d-%b-%Y")  # Assuming date format like "23-OCT-1996"
 
     if format == "unixtimestamp":
         return int(value.timestamp()*1000)
     else:
-        return value.strftime("%H:%M %d-%m-%y")
+        return value
+
+
+def get_value(key, dictionary):
+    print("{}: {}".format(dictionary, key))
+    return dictionary.get(key, '')
 
 
 def execute():
-    date_to_format = "01-DEC-2024"
+    row_data = ("01-DEC-2024", "01-DEC-2024", "abc")
+    company_dict = {
+        "def": "def_c",
+        "abc": "abc_c"
+    }
     env = Environment()
-    env.filters["datetimeformat"] = datetime_format
+    env.filters["format_date_time"] = format_date_time
+    env.filters["dict_value"] = get_value
     person_json = """{
-            "date": "{{ data|datetimeformat("unixtimestamp") }}"
+            "var1": "{{ data[0]|format_date_time("unixtimestamp") }}",
+            "var2": "{{ data[1]|format_date_time }}",
+            "var3": "{{ data[2]|dict_value(company_dict) }}" 
     }"""
-    # template = Template(person_json)
-    # template.globals['datetime_format'] = datetime_format
-    # rendered_form = (template.render(data=date_to_format))
     template = env.from_string(person_json)
-    rendered_form = template.render(data=date_to_format)
+    rendered_form = template.render(data=row_data, company_dict=company_dict)
     print(rendered_form)
 
+
+company_dict = '''{
+        "def": "def_c",
+        "abc": "abc_c"
+    }'''
 execute()
